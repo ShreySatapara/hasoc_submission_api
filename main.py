cb4f39acc94116309a1fcc0ecbe62c5ecc37e140
ShreySatapara <shreysatapara@gmail.com>
2021-07-28 15:56:08 +0530

    typo fixed at main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, request
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-@app.route('/home')
+
+@app.route('/')
 def test():
     return 'hello!!'
 
